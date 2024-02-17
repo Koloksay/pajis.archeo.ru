e69695e514c08b57ab1b1953d021d48fa4ccb79b
Katrechko Aleksandr <129851886+Koloksay@users.noreply.github.com>
2024-02-17 22:08:50 +0300

    Update and rename Main.py to main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from selenium import webdriver
-import Data
-import Method
+import data
+import method
 
 
 selector = 'div.issue-page__details > section'
 driver = webdriver.Chrome()
 
 # Обрабатываем каждую пару старого и нового URL-адресов
-for pair in Data.data:
+for pair in data.data:
     old_url, new_url = pair.values()
 
     # Открываем старую страницу, собираем данные
-    old_data = Method.extract_data_from_page(driver, old_url, selector)
-    new_data = Method.extract_data_from_page(driver, new_url, selector)
+    old_data = method.extract_data_from_page(driver, old_url, selector)
+    new_data = method.extract_data_from_page(driver, new_url, selector)
     print(f"\nСравниваем {old_url} и {new_url}")
-    Method.compare_data(old_data, new_data)
+    method.compare_data(old_data, new_data)
 
 driver.quit()
